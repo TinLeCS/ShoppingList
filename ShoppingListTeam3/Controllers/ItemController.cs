b9032fb96d3c1b022d71d558dfe9ae0ee857521f
LeaderTNT <trong.tin.le.cs@gmail.com>
2016-06-27 12:38:59 -0400

    Add project files.

---
+++ ShoppingListTeam3/Controllers/ItemController.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingListTeam3.Data;
+using ShoppingListTeam3.Models;
+using ShoppingListTeam3.Services;
+using PagedList;
+
+namespace ShoppingListTeam3.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly Lazy<ItemService> _svc = new Lazy<ItemService>();
+        private readonly Lazy<NoteService> _noteSvc = new Lazy<NoteService>();
+
+        // GET: Item
+        public ActionResult Index(int? id, int? shoppingListID, int? page)
+        {
+            {
+                var viewModel = new ItemWithNoteViewModel();
+
+                ViewBag.shoppingListID = id;
+                int pageSize = 5;
+                int pageNumber = page ?? 1;
+
+                viewModel.Items = _svc.Value.GetItemsByShoppingListID(id.Value).ToPagedList(pageNumber, pageSize);
+                if (shoppingListID != null)
+                {
+                    ViewBag.itemID = id.Value;
+                    ViewBag.shoppingListID = shoppingListID.Value;
+                    viewModel.Items = _svc.Value.GetItemsByShoppingListID(shoppingListID.Value).ToPagedList(pageNumber, pageSize);
+                    viewModel.Note = _noteSvc.Value.GetNoteByItemID(id.Value);
+                }
+                    
+                return View(viewModel);
+            }
+        }
+
+        // GET: Item/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemViewModel itemViewModel = _svc.Value.GetItemByID(id);
+            if (itemViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemViewModel);
+        }
+
+        // GET: Item/Create
+        public ActionResult Create(int shoppingListID)
+        {
+            ViewBag.shoppingListID = shoppingListID;
+            return View(new ItemViewModel());
+        }
+
+        // POST: Item/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Content,Priority,IsChecked")] ItemViewModel itemViewModel, int shoppingListID)
+        {
+            if (ModelState.IsValid)
+            {
+                _svc.Value.CreateItem(itemViewModel, shoppingListID);
+                return RedirectToAction("Index", new { id = shoppingListID });
+            }
+
+            return View(itemViewModel);
+        }
+
+        // GET: Item/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemViewModel itemViewModel = _svc.Value.GetItemByID(id);
+            ViewBag.shoppingListID = itemViewModel.ShoppingListID;
+            if (itemViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemViewModel);
+        }
+
+        // POST: Item/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Content,Priority,IsChecked")] ItemViewModel itemViewModel, int shoppingListID)
+        {
+            if (ModelState.IsValid)
+            {
+                _svc.Value.UpdateItem(itemViewModel);
+                return RedirectToAction("Index", new { id = shoppingListID });
+            }
+            return View(itemViewModel);
+        }
+
+        // GET: Item/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            ItemViewModel itemViewModel = _svc.Value.GetItemByID(id);
+            ViewBag.shoppingListID = itemViewModel.ShoppingListID;
+            if (itemViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemViewModel);
+        }
+
+        // POST: Item/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id, int shoppingListID)
+        {
+            _svc.Value.DeleteItem(id);
+            return RedirectToAction("Index", new { id = shoppingListID });
+        }
+
+        public ActionResult Clear(int? shoppingListID)
+        {
+            if (shoppingListID == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            _svc.Value.DeleteAllItems(shoppingListID);
+            return RedirectToAction("Index", new { id = shoppingListID});
+        }
+    }
+
+}
+
+
---
+++ ShoppingListTeam3/Controllers/NoteController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Net;
+using System.Web.Mvc;
+using ShoppingListTeam3.Models;
+using ShoppingListTeam3.Services;
+
+namespace ShoppingListTeam3.Controllers
+{
+    public class NoteController : Controller
+    {
+        private readonly Lazy<NoteService> _svc = new Lazy<NoteService>();
+
+        // GET: Item/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NoteViewModel itemViewModel = _svc.Value.GetNoteByItemID(id);
+            if (itemViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemViewModel);
+        }
+
+        // GET: Item/Create
+        public ActionResult Create(int id, int shoppingListID)
+        {
+            ViewBag.shoppingListID = shoppingListID;
+            ViewBag.itemID = id;
+            return View();
+        }
+
+        // POST: Item/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ItemId,Body")] NoteViewModel noteViewModel, int id, int shoppingListID)
+        {
+            if (ModelState.IsValid)
+            {
+                _svc.Value.CreateNote(noteViewModel, id);
+                return RedirectToAction("../Item/Index", new { id = id, shoppingListID = shoppingListID});
+            }
+
+            return View(noteViewModel);
+        }
+
+        // GET: Item/Edit/5
+        public ActionResult Edit(int? id, int shoppingListID)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            NoteViewModel itemViewModel = _svc.Value.GetNoteByItemID(id);
+            ViewBag.itemID = id;
+            ViewBag.shoppingListID = shoppingListID;
+            if (itemViewModel == null)
+            {
+                return HttpNotFound();
+            }
+            return View(itemViewModel);
+        }
+
+        // POST: Item/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ItemId,Body")] NoteViewModel noteViewModel, int id, int shoppingListID)
+        {
+            if (ModelState.IsValid)
+            {
+                _svc.Value.UpdateNote(noteViewModel);
+                return RedirectToAction("../Item/Index", new { id = id, shoppingListID = shoppingListID});
+            }
+            return View(noteViewModel);
+        }
+
+        // GET: Item/Delete/5
+        public ActionResult Delete(int? id, int shoppingListID)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            _svc.Value.DeleteItem(id);
+            return RedirectToAction("../Item/Index", new { id = id, shoppingListID = shoppingListID });
+        }
+    }
+}
+
+
